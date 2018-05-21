1af8d5741907af61e2b0524638c2a00049d9b7be
Peter Cooke <picarogit@outlook.com>
2018-05-21 21:27:21 +0100

    Lecture 301: Converting a Character BP to C++. Increment 2

--- Source/FPS/Character/Mannequin.h
+++ Source/FPS/Character/Mannequin.h
@@ -6,26 +6,46 @@
 #include "GameFramework/Character.h"
 #include "Mannequin.generated.h"
 
+class AMyGun;
+
 UCLASS()
 class FPS_API AMannequin : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AMannequin();
+    // Sets default values for this character's properties
+    AMannequin();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
+    virtual void UnPossessed() override;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Setup")
+    TSubclassOf<AMyGun> GunBlueprint;
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon")
+    void PullTrigger();
+
+private:
+    /** Pawn mesh: 1st person view (arms; seen only by self) */
+    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+        class USkeletalMeshComponent* Mesh1P;
+
+    /** First person camera */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    class UCameraComponent* FirstPersonCameraComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    AMyGun* Gun;
+
 };
